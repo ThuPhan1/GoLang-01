aef38bf85a6219ce372ce056f51c79c3fe143d7f
ThuPahn <95139699+ThuPahn@users.noreply.github.com>
2022-12-21 11:24:52 +0700

    .go

--- slinces.go
+++ slinces.go
@@ -3,43 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	s := make([]string, 4)
-	fmt.Println(s)
 
-    s [0] = "j"
-    s [1] = "q"
-	s [2] = "k"
-	s [3] = "o"
-	fmt.Println("set:" ,s)
-	fmt.Println("get:", s[2])
-	fmt.Println("len:", len(s))
+    s := make([]string, 3)
+    fmt.Println("emp:", s)
 
-c := make([]string, len(s))
-copy(c,s)
-fmt.Println("cpy:", s)
-l := s[2:5]
-fmt.Println("sl1:", l)
+    s[0] = "a"
+    s[1] = "b"
+    s[2] = "c"
+    fmt.Println("set:", s)
+    fmt.Println("get:", s[2])
 
+    fmt.Println("len:", len(s))
 
-l = s[:6]
-fmt.Println("sl2:", l)
-l = s[2:]
-fmt.Println("sl3:", l)
+    s = append(s, "d")
+    s = append(s, "e", "f")
+    fmt.Println("apd:", s)
 
-t := []string{"a", "b", "c", "d", "e", "f", "q"}
-fmt.Println("dcl",t)
+    c := make([]string, len(s))
+    copy(c, s)
+    fmt.Println("cpy:", c)
 
-twoD := make([][]int, 10)
-for i := 0; i <10 ; i++ {
-	innerLen := i + 1
-	twoD[i]=make([]int, innerLen)
-	for  j := 0;  j < innerLen;  j++ {
-		twoD[i][j] = i + j
+    l := s[2:5]
+    fmt.Println("sl1:", l)
 
-		
-	}
-	
+    l = s[:5]
+    fmt.Println("sl2:", l)
+
+    l = s[2:]
+    fmt.Println("sl3:", l)
+
+    t := []string{"g", "h", "i"}
+    fmt.Println("dcl:", t)
+
+    twoD := make([][]int, 3)
+    for i := 0; i < 3; i++ {
+        innerLen := i + 1
+        twoD[i] = make([]int, innerLen)
+        for j := 0; j < innerLen; j++ {
+            twoD[i][j] = i + j
+        }
+    }
+    fmt.Println("2d: ", twoD)
 }
-fmt.Println("2d", twoD)
-}
-
